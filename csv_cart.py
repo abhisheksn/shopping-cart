ad4d41dd9f621d07cdfb5d295757c34b66488804
Abhishek Swaminathan <44006049+abhisheksn@users.noreply.github.com>
2021-07-21 22:45:21 -0400

    Added files and coded for CSV
    Added files and coded for CSV

--- csv_cart.py
+++ csv_cart.py
@@ -1,63 +1,9 @@
-# shopping_cart.py
 import os
+from pandas import read_csv
 from dotenv import load_dotenv  # source:https://github.com/theskumar/python-dotenv
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from datetime import datetime
-
-load_dotenv()
-
-#If running CSV uncomment lines 9 to 14
-#from pandas import read_csv
-
-#csv_filepath = "data/products.csv"
-
-#products_df = read_csv("data/products.csv")
-#products = products_df.to_dict("records")
-
-#If running CSV comment lines 19 to 60
-products = [
-    {"id": 1, "name": "Chocolate Sandwich Cookies",
-        "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id": 2, "name": "All-Seasons Salt", "department": "pantry",
-        "aisle": "spices seasonings", "price": 4.99},
-    {"id": 3, "name": "Robust Golden Unsweetened Oolong Tea",
-        "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id": 4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce",
-        "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id": 5, "name": "Green Chile Anytime Sauce", "department": "pantry",
-        "aisle": "marinades meat preparation", "price": 7.99},
-    {"id": 6, "name": "Dry Nose Oil", "department": "personal care",
-        "aisle": "cold flu allergy", "price": 21.99},
-    {"id": 7, "name": "Pure Coconut Water With Orange",
-        "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id": 8, "name": "Cut Russet Potatoes Steam N' Mash",
-        "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id": 9, "name": "Light Strawberry Blueberry Yogurt",
-        "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id": 10, "name": "Sparkling Orange Juice & Prickly Pear Beverage",
-       "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id": 11, "name": "Peach Mango Juice", "department": "beverages",
-        "aisle": "refrigerated", "price": 1.99},
-    {"id": 12, "name": "Chocolate Fudge Layer Cake",
-        "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id": 13, "name": "Saline Nasal Mist", "department": "personal care",
-        "aisle": "cold flu allergy", "price": 16.00},
-    {"id": 14, "name": "Fresh Scent Dishwasher Cleaner",
-        "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id": 15, "name": "Overnight Diapers Size 6",
-        "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id": 16, "name": "Mint Chocolate Flavored Syrup",
-        "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id": 17, "name": "Rendered Duck Fat", "department": "meat seafood",
-        "aisle": "poultry counter", "price": 9.99},
-    {"id": 18, "name": "Pizza for One Suprema Frozen Pizza",
-        "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id": 19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend",
-        "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id": 20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink",
-        "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-]  # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 
 def to_usd(my_price):
@@ -72,8 +18,12 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}"  # > $12,000.71
 
+csv_filepath = "data/products.csv"
 
-# Python code
+products_df = read_csv("data/products.csv")
+products = products_df.to_dict("records")
+
+load_dotenv()
 selected_ids = []
 total_price = 0
 
@@ -103,7 +53,8 @@ for selected_id in selected_ids:
   total_price = total_price + matching_prod["price"]
   tax = (total_price * float(tax_rate))
   Total = (total_price + tax)
-  print("+ " + matching_prod["name"] + " (" + to_usd(matching_prod["price"]) + ")")
+  print("+ " + matching_prod["name"] +
+        " (" + to_usd(matching_prod["price"]) + ")")
 print("---------------------------------")
 print("Subtotal:", to_usd(total_price))
 print("Tax:", to_usd(tax))
@@ -115,6 +66,7 @@ user_email = input("Please enter your email:")
 
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL")
+
 
 def send_email(subject="[Daily Briefing] This is a test", html="<p>Hello World</p>", recipient_address=SENDER_EMAIL_ADDRESS):
     """
@@ -139,6 +91,7 @@ def send_email(subject="[Daily Briefing] This is a test", html="<p>Hello World</
     except Exception as e:
         print("OOPS", type(e), e)
         return None
+
 
 if __name__ == "__main__":
  html = ""
