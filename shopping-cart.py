a7e797321528cad451d7d1e488974890c5813aac
Abhishek Swaminathan <44006049+abhisheksn@users.noreply.github.com>
2021-07-23 00:38:39 -0400

    Updated script
    Commented Google sheet code and uncommented hard-coded products list code. And added .DS_Store to gitignore

--- shopping-cart.py
+++ shopping-cart.py
@@ -16,71 +16,71 @@ load_dotenv()
 #products = products_df.to_dict("records")
 
 #If running from a Google Spreadsheet uncomment lines 19 to 38
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import requests
-import json
-
-DOCUMENT_ID = os.getenv("GOOGLE_SHEET_ID", default="OOPS")
-SHEET_NAME = os.getenv("SHEET_NAME", default="Products-2021")
-
-CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "google-credentials.json")
-AUTH_SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file"]
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
-
-client = gspread.authorize(credentials)
-
-doc = client.open_by_key(DOCUMENT_ID)
-
-sheet = doc.worksheet(SHEET_NAME)
-
-products = sheet.get_all_records()
+#import gspread
+#from oauth2client.service_account import ServiceAccountCredentials
+#import requests
+#import json
+#
+#DOCUMENT_ID = os.getenv("GOOGLE_SHEET_ID", default="OOPS")
+#SHEET_NAME = os.getenv("SHEET_NAME", default="Products-2021")
+#
+#CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "google-credentials.json")
+#AUTH_SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file"]
+#
+#credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+#
+#client = gspread.authorize(credentials)
+#
+#doc = client.open_by_key(DOCUMENT_ID)
+#
+#sheet = doc.worksheet(SHEET_NAME)
+#
+#products = sheet.get_all_records()
 
 #Hard Coded list of products
 #If running CSV comment or running product list from a Google Sheet lines 42 to 83
-#products = [
-#    {"id": 1, "name": "Chocolate Sandwich Cookies",
-#        "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-#    {"id": 2, "name": "All-Seasons Salt", "department": "pantry",
-#        "aisle": "spices seasonings", "price": 4.99},
-#    {"id": 3, "name": "Robust Golden Unsweetened Oolong Tea",
-#        "department": "beverages", "aisle": "tea", "price": 2.49},
-#    {"id": 4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce",
-#        "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-#    {"id": 5, "name": "Green Chile Anytime Sauce", "department": "pantry",
-#        "aisle": "marinades meat preparation", "price": 7.99},
-#    {"id": 6, "name": "Dry Nose Oil", "department": "personal care",
-#        "aisle": "cold flu allergy", "price": 21.99},
-#    {"id": 7, "name": "Pure Coconut Water With Orange",
-#        "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-#    {"id": 8, "name": "Cut Russet Potatoes Steam N' Mash",
-#        "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-#    {"id": 9, "name": "Light Strawberry Blueberry Yogurt",
-#        "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-#    {"id": 10, "name": "Sparkling Orange Juice & Prickly Pear Beverage",
-#       "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-#    {"id": 11, "name": "Peach Mango Juice", "department": "beverages",
-#        "aisle": "refrigerated", "price": 1.99},
-#    {"id": 12, "name": "Chocolate Fudge Layer Cake",
-#        "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-#    {"id": 13, "name": "Saline Nasal Mist", "department": "personal care",
-#        "aisle": "cold flu allergy", "price": 16.00},
-#    {"id": 14, "name": "Fresh Scent Dishwasher Cleaner",
-#        "department": "household", "aisle": "dish detergents", "price": 4.99},
-#    {"id": 15, "name": "Overnight Diapers Size 6",
-#        "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-#    {"id": 16, "name": "Mint Chocolate Flavored Syrup",
-#        "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-#    {"id": 17, "name": "Rendered Duck Fat", "department": "meat seafood",
-#        "aisle": "poultry counter", "price": 9.99},
-#    {"id": 18, "name": "Pizza for One Suprema Frozen Pizza",
-#        "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-#    {"id": 19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend",
-#        "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-#    {"id": 20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink",
-#        "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-#]  # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+products = [
+    {"id": 1, "name": "Chocolate Sandwich Cookies",
+        "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id": 2, "name": "All-Seasons Salt", "department": "pantry",
+        "aisle": "spices seasonings", "price": 4.99},
+    {"id": 3, "name": "Robust Golden Unsweetened Oolong Tea",
+        "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id": 4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce",
+        "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id": 5, "name": "Green Chile Anytime Sauce", "department": "pantry",
+        "aisle": "marinades meat preparation", "price": 7.99},
+    {"id": 6, "name": "Dry Nose Oil", "department": "personal care",
+        "aisle": "cold flu allergy", "price": 21.99},
+    {"id": 7, "name": "Pure Coconut Water With Orange",
+        "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id": 8, "name": "Cut Russet Potatoes Steam N' Mash",
+        "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id": 9, "name": "Light Strawberry Blueberry Yogurt",
+        "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id": 10, "name": "Sparkling Orange Juice & Prickly Pear Beverage",
+       "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id": 11, "name": "Peach Mango Juice", "department": "beverages",
+        "aisle": "refrigerated", "price": 1.99},
+    {"id": 12, "name": "Chocolate Fudge Layer Cake",
+        "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id": 13, "name": "Saline Nasal Mist", "department": "personal care",
+        "aisle": "cold flu allergy", "price": 16.00},
+    {"id": 14, "name": "Fresh Scent Dishwasher Cleaner",
+        "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id": 15, "name": "Overnight Diapers Size 6",
+        "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id": 16, "name": "Mint Chocolate Flavored Syrup",
+        "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id": 17, "name": "Rendered Duck Fat", "department": "meat seafood",
+        "aisle": "poultry counter", "price": 9.99},
+    {"id": 18, "name": "Pizza for One Suprema Frozen Pizza",
+        "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id": 19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend",
+        "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id": 20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink",
+        "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+]  # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 #Converting numeric value to USD formatted string
 #source: Professor Rossetti
